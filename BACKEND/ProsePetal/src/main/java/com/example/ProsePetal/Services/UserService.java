80f52ef7a1eb491a86ef4d4755e26da37a9cdde5
AnkitaAgrawal12 <155989679+AnkitaAgrawal12@users.noreply.github.com>
2024-05-14 19:06:49 +0530

    Delete BACKEND directory

--- BACKEND/ProsePetal/src/main/java/com/example/ProsePetal/Services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProsePetal.Services;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UserService {
-    Object getprofiledetails(Integer userId);
-}
