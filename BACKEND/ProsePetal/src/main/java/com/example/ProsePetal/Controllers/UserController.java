35d1f3f6af686bd174d62523c10d9c2d283d267d
AnkitaAgrawal12 <AnkitaAgrawal@iiitb.ac.in>
2024-05-14 19:20:02 +0530

    commit

---
+++ BACKEND/ProsePetal/src/main/java/com/example/ProsePetal/Controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.example.ProsePetal.Controllers;
+
+import com.example.ProsePetal.Payloads.PostDTO;
+import com.example.ProsePetal.Payloads.UserDTO;
+import com.example.ProsePetal.Services.PostService;
+import com.example.ProsePetal.Services.UserService;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@NoArgsConstructor
+public class UserController {
+    private PostService postService;
+    private UserService userService;
+
+
+
+    @GetMapping("/viewProfile/{userId}")
+    public ResponseEntity<UserDTO> profiledetails(@PathVariable("userId") Integer userId)
+    {
+        UserDTO user = (UserDTO) userService.getprofiledetails(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+}
