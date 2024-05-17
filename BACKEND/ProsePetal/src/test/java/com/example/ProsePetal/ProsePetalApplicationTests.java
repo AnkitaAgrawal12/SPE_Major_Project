444189511ab111e3e423ad64917fb0b3fb3dc63c
AnkitaAgrawal12 <AnkitaAgrawal@iiitb.ac.in>
2024-05-17 08:12:19 +0530

    commit

--- BACKEND/ProsePetal/src/test/java/com/example/ProsePetal/ProsePetalApplicationTests.java
+++ BACKEND/ProsePetal/src/test/java/com/example/ProsePetal/ProsePetalApplicationTests.java
@@ -1,38 +1,13 @@
 package com.example.ProsePetal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-
-import com.example.ProsePetal.Entity.Post;
-import com.example.ProsePetal.Entity.User;
-import com.example.ProsePetal.Services.PostService;
-import com.example.ProsePetal.Services.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(MockitoExtension.class)
-public class ProsePetalApplicationTests {
-
-	@Mock
-	private UserService userService;
-
-	@InjectMocks
-	private PostService postService;
-
-	@BeforeEach
-	public void setUp() {
-		when(userService.getUserById(anyLong())).thenAnswer(invocation -> {
-			Integer userId = invocation.getArgument(0);
-			return new User(userId);
-		});
-	}
+@SpringBootTest
+class ProsePetalApplicationTests {
 
 	@Test
-	public void testCreatePost() {
-
+	void contextLoads() {
 	}
+
 }
